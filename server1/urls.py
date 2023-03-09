26aff443c6ce0c0061a0c5889eaacf020c407612
kalagarabhaskar <kalagarabhaskaraoking95@gmail.com>
2023-03-09 19:45:03 +0530

    first commit

---
+++ server1/urls.py
@@ -0,0 +1,8 @@
+from server1.views import *
+from django.urls import path
+app_name='something'
+urlpatterns=[
+
+path('jango/',jango,name='jango'),
+
+]
---
+++ server2/urls.py
@@ -0,0 +1,9 @@
+from server2.views import *
+from django.urls import path
+app_name='nothing'
+urlpatterns=[
+
+path('jango1/',jango1,name='jango1'),
+
+
+]
